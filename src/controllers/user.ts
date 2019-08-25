cc8c0a11f388f63032248c39e8b296b04c87cc28
bkawk <email.will.in.china@gmail.com>
2019-08-25 16:05:34 +0800

    lint

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,23 +1,22 @@
-
-import { Request, Response, NextFunction } from 'express';
 import * as Joi from '@hapi/joi';
 import * as bcrypt from 'bcrypt';
-import UserModel, { IUser } from '../models/users';
-import * as jwt from 'jsonwebtoken';
 import {PrivateKey} from 'eosjs-ecc';
+import {NextFunction,  Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import UserModel, { IUser } from '../models/users';
 
 class User {
 
   static validateLogin = async (req: Request, res: Response, next: NextFunction) => {
     const schema = Joi.object().keys({
       email: Joi.string().email({ minDomainSegments: 2 }),
-      password: Joi.string().min(5)
-    })
+      password: Joi.string().min(5),
+    });
     const email = req.body.email;
     const password = req.body.password;
-    Joi.validate({ email, password }, schema, (err) => { 
-      if(!err) next(); else res.json(err.details)
-    });  
+    Joi.validate({ email, password }, schema, (err) => {
+      if (!err) next(); else res.json(err.details);
+    });
   };
 
   static login = async (req: Request, res: Response) => {
@@ -27,14 +26,14 @@ class User {
     const userId = account._id;
     const apiKey = account.apiKey;
     if (!account) {
-      res.status(400).send({ message: "You have not registered" });
+      res.status(400).send({ message: 'You have not registered' });
     } else {
       const passwordsMatch = await bcrypt.compare(password, account.password);
       if (!passwordsMatch) {
-        res.status(400).send({ message: "Incorrect password" });
+        res.status(400).send({ message: 'Incorrect password' });
       } else {
         const jwtToken = jwt.sign({ email, userId }, process.env.JWT_SECRET);
-        res.status(200).send({ message: "You are now logged-in", jwtToken, apiKey });
+        res.status(200).send({ message: 'You are now logged-in', jwtToken, apiKey });
       }
     }
   };
@@ -42,45 +41,44 @@ class User {
   static validateRegister = async (req: Request, res: Response, next: NextFunction) => {
     const schema = Joi.object().keys({
       email: Joi.string().email({ minDomainSegments: 2 }),
-      password: Joi.string().min(5)
-    })
+      password: Joi.string().min(5),
+    });
     const email = req.body.email;
     const password = req.body.password;
-    Joi.validate({ email,password }, schema, (err) => { 
-      if(!err) next(); else res.json(err.details)
-    });  
+    Joi.validate({ email, password }, schema, (err) => {
+      if (!err) next(); else res.json(err.details);
+    });
   };
-
 
   static register = async (req: Request, res: Response) => {
     const email = req.body.email;
     const password = req.body.password;
     const alreadyRegistered = await UserModel.findOne({email}).exec();
     if (!alreadyRegistered) {
-      const hashedPassword = await bcrypt.hash(password,10);
-        if (!hashedPassword) {
-          res.status(500).send({ message: "Failed to encrypt your password" });
+      const hashedPassword = await bcrypt.hash(password, 10);
+      if (!hashedPassword) {
+        res.status(500).send({ message: 'Failed to encrypt your password' });
+      } else {
+        const privateKey = await PrivateKey.randomKey();
+        const apiKey = await privateKey.toWif();
+        const validKey = PrivateKey.isValid(apiKey);
+        if (!validKey) {
+          res.status(500).send({ message: 'Generated api key invalid' });
         } else {
-          const privateKey = await PrivateKey.randomKey();
-          const apiKey = await privateKey.toWif();
-          const validKey = PrivateKey.isValid(apiKey);
-          if (!validKey) {
-            res.status(500).send({ message: "Generated api key invalid" });
+          const user = new UserModel({email, password: hashedPassword, apiKey} as IUser);
+          const saved = await user.save();
+          if (!saved) {
+            res.status(500).send({ message: 'Failed to register you' });
           } else {
-            const user = new UserModel(<IUser>{email:email, password:hashedPassword, apiKey});
-            const saved = await user.save();
-            if (!saved) {
-              res.status(500).send({ message: "Failed to register you" });
-            } else {
-              res.status(200).send({ message: "You are now registered", apiKey });
-            }
+            res.status(200).send({ message: 'You are now registered', apiKey });
           }
         }
+      }
     } else {
-      res.status(400).send({ message: "You have already registered" });
+      res.status(400).send({ message: 'You have already registered' });
     }
   };
 
 }
 
-export {User} 
+export {User};
--- src/server.ts
+++ src/server.ts
@@ -1,74 +1,89 @@
-import * as dotenv from "dotenv"
-import * as path from "path";
-import * as express from 'express'
-import * as bodyParser from 'body-parser'
-import * as cors from 'cors'
-import * as helmet from 'helmet'
-import * as morgan from 'morgan'
+import * as bluebird from 'bluebird';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
+import * as express from 'express';
+import * as helmet from 'helmet';
+import * as http from 'http';
 import * as mongoose from 'mongoose';
-import * as bluebird from "bluebird";
+import * as morgan from 'morgan';
+import * as path from 'path';
+import { RateLimiterMongo } from 'rate-limiter-flexible';
+import * as socketIo from 'socket.io';
 import { router } from './router';
 
 // Set env values
 dotenv.config();
 
 // Connect to MongoDB
-(<any>mongoose).Promise = bluebird;
+declare module 'mongoose' {
+  type Promise<T> = bluebird<T>;
+}
+// tslint:disable-next-line: max-line-length
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true`, {useCreateIndex: true, useNewUrlParser: true});
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${process.env.DB_NAME}`);
 });
 
 // Configure Rate Limiter
-const { RateLimiterMongo } = require('rate-limiter-flexible');
-const rateLimiterMongo = new RateLimiterMongo({storeClient: mongoose.connection, points: 4, duration: 1}); // 4 request in 1 second per ip address
-const rateLimiter = (req: any, res: any, next: any) => {
+const rateLimiterMongo = new RateLimiterMongo({storeClient: mongoose.connection, points: 4, duration: 1});
+const rateLimiter = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   rateLimiterMongo.consume(req.ip)
     .then(() => next())
     .catch(() => res.status(429).send('Whoa! Slow down there little buddy'));
 };
 
 // Configure CORS
-const corsOptions = {
-  origin: (origin: any, callback: any) => {
-    if (process.env.CORS_WHITELIST && process.env.CORS_WHITELIST.indexOf(origin) !== -1) callback(null, true);
-    else callback('Not allowed by CORS');
-  },
-  allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], optionsSuccessStatus: 200,
+const options: cors.CorsOptions = {
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token'],
+  credentials: true,
+  methods: 'GET, HEAD, OPTIONS, PUT, PATCH, POST, DELETE',
+  origin: (origin, callback) => callback(null, process.env.CORS_WHITELIST.indexOf(origin) !== -1),
+  preflightContinue: false,
 };
 
-// Configure App
+// Configure Server
 const app = express();
+const server = new http.Server(app);
+const io = socketIo(server);
+
+// Configure App
 app.use(helmet());
 app.use(morgan('dev'));
-app.use(cors(corsOptions));
+app.use(cors(options));
+app.options('*', cors(options));
 app.use(rateLimiter);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Catch Syntax Error in JSON
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+interface Error {
+  status?: number;
+}
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
   if (err.status === 400 && err instanceof SyntaxError && 'body' in err) {
-    res.status(200).send({ message: "JSON Syntax Error" });
+    res.status(200).send({ message: 'JSON Syntax Error' });
   } else {
     next();
   }
 });
 app.use('/', router);
 
-let http = require("http").Server(app);
-require('./sockets')(require("socket.io")(http));
+io.on('connection', (socket) => {
+  // tslint:disable-next-line: no-console
+  console.log('Socket Connected');
+});
 
 // Start Server
 const port = process.env.API_PORT;
-http.listen(port, ()=> {
+server.listen(port, () => {
+  // tslint:disable-next-line: no-console
   console.log(`listening on ${port}`);
 });
 
 // Serve Socket test page
-app.get("/socket", (req: any, res: any) => {
-    res.sendFile(path.resolve("./src/client/index.html"));
-  });
+app.get('/socket', (req: express.Request, res: express.Response) => {
+  res.sendFile(path.resolve('./src/client/index.html'));
+});
 
 export {app};
